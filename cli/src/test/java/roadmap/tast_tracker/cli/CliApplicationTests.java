6c70660cd1e00bb138b1958b470313bdf827bfa9
Garyn14 <garyn.fernando.hr@gmail.com>
2024-09-14 21:03:50 -0500

    inicio del proyecto

---
+++ cli/src/test/java/roadmap/tast_tracker/cli/CliApplicationTests.java
@@ -0,0 +1,13 @@
+package roadmap.tast_tracker.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
